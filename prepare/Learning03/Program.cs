f200ae96b6b2117ceea3410e1e93c2f3316c86b9
Diego Vargas <diego.vargasd@gmail.com>
2024-03-11 23:30:11 -0600

    Built W03 Prepare: Learning Activity - Fraction

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        // Setting values for f1 - Should be blank
+        Fraction f = new Fraction();
+        Console.WriteLine(f.GetFractionString());
+        Console.WriteLine(f.GetDecimalValue());
+
+        // Setting values for f2 - 5 & 1
+        f.SetTop(5);
+        f.SetBottom(1);
+        Console.WriteLine(f.GetFractionString());
+        Console.WriteLine(f.GetDecimalValue());
+
+        // Setting values for f3 - 3 & 4
+        f.SetTop(3);
+        f.SetBottom(4);
+        Console.WriteLine(f.GetFractionString());
+        Console.WriteLine(f.GetDecimalValue());
+
+        // Setting values for f4 - 1 & 3
+        f.SetTop(1);
+        f.SetBottom(3);
+        Console.WriteLine(f.GetFractionString());
+        Console.WriteLine(f.GetDecimalValue());
+
+        // Setting values for f5 - 2 & 0
+        //f.SetTop(2);
+        //f.SetBottom(0);
+        //Console.WriteLine(f.GetFractionString());
+        //Console.WriteLine(f.GetDecimalValue());
     }
 }
