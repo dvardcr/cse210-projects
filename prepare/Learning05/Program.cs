2c7aadfc6c98925988b4760bd60c73e687a891a3
Diego Vargas <diego.vargasd@gmail.com>
2024-03-25 23:06:06 -0600

    Completed Polymorphism Learning Activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,30 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        //Shape squareShape = new Square("green", 4);
+
+        //string squareColor = squareShape.GetColor();
+        //Console.WriteLine($"Square Color: {squareColor}");
+        
+        //double squareArea = squareShape.GetArea();
+        //Console.WriteLine($"Square Area: {squareArea}");
+
+    List<Shape> shapes = new List<Shape>();
+
+    shapes.Add(new Square("red", 4));
+    shapes.Add(new Rectangle("blue", 4, 4));
+    shapes.Add(new Circle("yellow", 4));
+
+    foreach (Shape shape in shapes)
+        {
+            Console.WriteLine("Color: " + shape.GetColor());
+            Console.WriteLine("Area: " + shape.GetArea());
+            Console.WriteLine();
+            //Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
